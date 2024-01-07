3bc8fbbcb546d72c40254c59961a04287ae58382
Eric Hine <eric.hine2214@gmail.com>
2024-01-07 15:15:45 -0500

    Add SearchInsertPosition; Easy/Array

---
+++ src/test/java/org/hine/easy/SearchInsertPositionTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SearchInsertPositionTest {
+
+    @Test
+    void testSearchInsertPosition() {
+        int[] nums1 = {1, 2, 3, 4, 5, 6, 7};
+        int target1 = 2, expected1 = 1;
+        Assertions.assertEquals(expected1, new SearchInsertPosition().searchInsertPosition(nums1, target1));
+
+        int[] nums2 = {1, 2, 4, 5, 6, 7};
+        int target2 = 3, expected2 = 2;
+        Assertions.assertEquals(expected2, new SearchInsertPosition().searchInsertPosition(nums2, target2));
+    }
+}
