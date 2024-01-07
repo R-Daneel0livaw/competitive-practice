eca37c73d3f068c076e06f22d4f80e39233a1797
Eric Hine <eric.hine2214@gmail.com>
2024-01-07 10:43:50 -0500

    Add TwoSum; Easy/Array

---
+++ src/main/java/org/hine/easy/TwoSum.java
@@ -0,0 +1,20 @@
+package org.hine.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(complements.containsKey(complement)) {
+                return new int[] { complements.get(complement), i };
+            } else {
+                complements.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
