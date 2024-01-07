96be655e20d2a146208dc72297a84131155a9a3e
Eric Hine <eric.hine2214@gmail.com>
2024-01-07 11:03:18 -0500

    Add RemoveDuplicates; Easy/Array

---
+++ src/main/java/org/hine/easy/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+package org.hine.easy;
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        int insertIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
