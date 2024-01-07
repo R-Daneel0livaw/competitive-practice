609f6accdfb5ece4f2d1f8bf0ad538c1c7c336e3
Eric Hine <eric.hine2214@gmail.com>
2024-01-07 15:30:59 -0500

    Add PlusOne; Easy/Array

---
+++ src/test/java/org/hine/easy/PlusOneTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlusOneTest {
+
+    @Test
+    void testPlusOne() {
+        int[] digits1 = {1, 0, 0, 2};
+        int[] expected1 = {1, 0, 0, 3};
+        Assertions.assertArrayEquals(expected1, new PlusOne().plusOne(digits1));
+
+        int[] digits2 = {9, 9, 9, 9};
+        int[] expected2 = {1, 0, 0, 0, 0};
+        Assertions.assertArrayEquals(expected2, new PlusOne().plusOne(digits2));
+    }
+}
