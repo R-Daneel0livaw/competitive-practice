77c0c03b44036d6ab2e1934f9c98882bc5f890fd
Eric Hine <eric.hine2214@gmail.com>
2024-01-07 11:16:22 -0500

    Add RemoveElement; Easy/Array

---
+++ src/main/java/org/hine/easy/RemoveElement.java
@@ -0,0 +1,15 @@
+package org.hine.easy;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int insertIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
---
+++ src/test/java/org/hine/easy/RemoveElementTest.java
@@ -0,0 +1,20 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RemoveElementTest {
+
+    @Test
+    void testRemoveElement() {
+        int[] nums1 = {4, 3, 2, 1, 3, 1};
+        int val1 = 3;
+        int expected1 = 4;
+        Assertions.assertEquals(expected1, new RemoveElement().removeElement(nums1, val1));
+
+        int[] nums2 = {4, 2, 1, 1};
+        int val2 = 3;
+        int expected2 = 4;
+        Assertions.assertEquals(expected2, new RemoveElement().removeElement(nums2, val2));
+    }
+}
