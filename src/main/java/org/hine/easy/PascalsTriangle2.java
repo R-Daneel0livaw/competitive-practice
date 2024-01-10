c6c40c563dc0c9c2d548ccd9b2f6ecdace07682c
Eric Hine <eric.hine2214@gmail.com>
2024-01-09 19:15:43 -0500

    Add PascalsTriangle2; Easy/Array

---
+++ src/main/java/org/hine/easy/PascalsTriangle2.java
@@ -0,0 +1,23 @@
+package org.hine.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle2 {
+
+    public List<Integer> pascalsTriangle2(int rowIndex) {
+        List<Integer> row = new ArrayList<>(rowIndex + 1) {
+            {
+                add(1);
+            }
+        };
+
+        for (int i = 0; i < rowIndex; i++) {
+            for (int j = i; j > 0 ; j--) {
+                row.set(j, row.get(j - 1) + row.get(j));
+            }
+            row.add(1);
+        }
+        return row;
+    }
+}
---
+++ src/test/java/org/hine/easy/PascalsTriangle2Test.java
@@ -0,0 +1,30 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class PascalsTriangle2Test {
+
+    @Test
+    void testPascalsTriangle2() {
+        int rowIndex1 = 0;
+        List<Integer> expected1 = new ArrayList<>() {
+            {
+                add(1);
+            }
+        };
+        assertArrayEquals(expected1.toArray(), new PascalsTriangle2().pascalsTriangle2(rowIndex1).toArray());
+
+        int rowIndex2 = 3;
+        List<Integer> expected2 = new ArrayList<>();
+        expected2.add(1);
+        expected2.add(3);
+        expected2.add(3);
+        expected2.add(1);
+        assertArrayEquals(expected2.toArray(), new PascalsTriangle2().pascalsTriangle2(rowIndex2).toArray());
+    }
+}
