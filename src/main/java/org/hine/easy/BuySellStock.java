9490ed07839ca6ca52c4b9239a89807d357a3c4c
Eric Hine <eric.hine2214@gmail.com>
2024-01-09 19:56:51 -0500

    Add BuySellStock; Easy/Array

---
+++ src/main/java/org/hine/easy/BuySellStock.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+public class BuySellStock {
+
+    public int buySellStock(int[] nums) {
+       int minPrice = Integer.MAX_VALUE;
+       int maxProfit = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(minPrice > nums[i]) {
+                minPrice = nums[i];
+            } else if (maxProfit < nums[i] - minPrice) {
+                maxProfit = nums[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/test/java/org/hine/easy/BuySellStockTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuySellStockTest {
+
+    @Test
+    void testBuySellStock() {
+        int[] nums1 = {7, 1, 5, 3, 6, 4};
+        int expected1 = 5;
+        assertEquals(expected1, new BuySellStock().buySellStock(nums1));
+
+        int[] nums2 = {6, 5, 4, 3, 2, 1};
+        int expected2 = 0;
+        assertEquals(expected2, new BuySellStock().buySellStock(nums2));
+    }
+}
