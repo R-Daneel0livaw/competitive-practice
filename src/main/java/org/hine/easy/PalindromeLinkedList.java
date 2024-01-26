b1db19eac2aaa0ae01af0bbf4a7586458c0474fe
Eric Hine <eric.hine2214@gmail.com>
2024-01-26 18:51:00 -0500

    Add PalindromeLinkedList.

---
+++ src/main/java/org/hine/easy/PalindromeLinkedList.java
@@ -0,0 +1,22 @@
+package org.hine.easy;
+
+import org.hine.easy.util.ListNode;
+
+public class PalindromeLinkedList {
+
+    ListNode p;
+
+    public boolean palindromeLinkedList(ListNode head) {
+        p = head;
+        return recursivelyCheck(head);
+    }
+
+    private boolean recursivelyCheck(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!recursivelyCheck(currentNode.next)) return false;
+            if (currentNode.val != p.val) return false;
+            p = p.next;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/hine/easy/PalindromeLinkedListTest.java
@@ -0,0 +1,21 @@
+package org.hine.easy;
+
+import org.hine.easy.util.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PalindromeLinkedListTest {
+
+    @Test
+    void testPalindromeLinkedList() {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+        boolean expected = true;
+
+        ListNode head2 = new ListNode(1, new ListNode(2, new ListNode(2)));
+        boolean expected2 = false;
+
+        assertEquals(expected, new PalindromeLinkedList().palindromeLinkedList(head));
+        assertEquals(expected2, new PalindromeLinkedList().palindromeLinkedList(head2));
+    }
+}
