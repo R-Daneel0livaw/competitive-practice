03dd4cb4dd50ac5687d0520b1649158ac36f839d
Eric Hine <eric.hine2214@gmail.com>
2024-01-27 13:34:14 -0500

    Add IntersectionLinkedList.

---
+++ src/test/java/org/hine/easy/IntersectionLinkedListTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.hine.easy.util.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntersectionLinkedListTest {
+
+    @Test
+    void testIntersectionLinkedList() {
+        ListNode intersection = new ListNode(5);
+        ListNode head1 = new ListNode(1, new ListNode(2, new ListNode(3, intersection)));
+        ListNode head2 = new ListNode(4, intersection);
+
+        assertEquals(intersection.val, new IntersectionLinkedList().intersectionLinkedList(head1, head2).val);
+    }
+}
