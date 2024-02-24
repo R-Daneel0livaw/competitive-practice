24c76c800dd5a05bfe561e842373e748ac219c89
Eric Hine <eric.hine2214@gmail.com>
2024-02-24 17:57:04 -0500

    Add Majority Element.

---
+++ src/main/java/org/hine/easy/MajorityElement.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+public class MajorityElement {
+
+    public Integer majorityElement(int[] arr) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : arr) {
+           if (count == 0) {
+               candidate = num;
+           }
+           count = num == candidate ? count  + 1 : count - 1;
+        }
+        return candidate;
+    }
+}
---
+++ src/test/java/org/hine/easy/MajorityElementTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MajorityElementTest {
+
+    @Test
+    void testMajorityElement() {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int expected = 2;
+        int actual = new MajorityElement().majorityElement(arr);
+
+        assertEquals(expected, actual);
+    }
+}
