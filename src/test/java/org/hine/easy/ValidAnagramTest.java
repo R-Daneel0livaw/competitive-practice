e99b332c71a3a0292eb2a0c7c02388e29d036398
Eric Hine <eric.hine2214@gmail.com>
2024-02-25 15:01:41 -0500

    Add Valid Anagram; Easy/Hash Table.

---
+++ src/test/java/org/hine/easy/ValidAnagramTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidAnagramTest {
+
+    @Test
+    void testValidAnagram() {
+        String s = "anagram";
+        String t = "nagaram";
+
+        boolean expected = true;
+        boolean actual = new ValidAnagram().validAnagram(s, t);
+
+        assertEquals(expected, actual);
+    }
+}
