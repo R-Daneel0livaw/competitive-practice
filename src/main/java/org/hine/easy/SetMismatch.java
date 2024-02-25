d37ef6432d803b6c458fda2cd738dff75082fbb1
Eric Hine <eric.hine2214@gmail.com>
2024-02-25 16:30:17 -0500

    Add SetMismatch; Easy/Hash Table.

---
+++ src/main/java/org/hine/easy/SetMismatch.java
@@ -0,0 +1,27 @@
+package org.hine.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetMismatch {
+
+    public int[] setMismatch(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int dup = -1;
+        int missing = 1;
+        for (int num : nums) {
+           map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(i)) {
+                if(map.get(i) == 2) {
+                    dup = i;
+                }
+            } else {
+                missing = i;
+            }
+        }
+        return new int[]{dup, missing};
+    }
+}
---
+++ src/test/java/org/hine/easy/SetMismatchTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SetMismatchTest {
+
+    @Test
+    void testSetMismatch(){
+        int[] nums = {1, 2, 2, 4};
+
+        int[] expected = {2, 3};
+        int[] actual = new SetMismatch().setMismatch(nums);
+
+        assertArrayEquals(expected, actual);
+    }
+}
