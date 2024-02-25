b09eedd0a15cac890fdcebe459499f4e955cc5f3
Eric Hine <eric.hine2214@gmail.com>
2024-02-25 15:39:44 -0500

    Add ArrayIntersection; Easy/Hash Table.

---
+++ src/test/java/org/hine/easy/ArrayIntersectionTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ArrayIntersectionTest {
+
+    @Test
+    void testArrayIntersectionTest() {
+        int[] arr1 = {4, 9, 5};
+        int[] arr2 = {9, 4, 9, 8, 4};
+
+        int[] expected = {4, 9};
+        int[] actual = new ArrayIntersection().arrayIntersection(arr1, arr2);
+
+        assertArrayEquals(expected, actual);
+    }
+}
