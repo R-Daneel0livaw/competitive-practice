3e8a0868f203a3f1c0dd2f04e0b2fc9da20eea09
Eric Hine <eric.hine2214@gmail.com>
2024-02-26 16:12:23 -0500

    Add Logger; Easy/Hash Table.

---
+++ src/main/java/org/hine/easy/Logger.java
@@ -0,0 +1,24 @@
+package org.hine.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Logger {
+
+    Map<String, Integer> msgDict = new HashMap<>();
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!msgDict.containsKey(message)) {
+            msgDict.put(message, timestamp);
+            return true;
+        }
+
+        Integer oldTimestamp = this.msgDict.get(message);
+        int differnce = timestamp - oldTimestamp;
+        if(differnce >= 10) {
+            this.msgDict.put(message, timestamp);
+            return  true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/org/hine/easy/LoggerTest.java
@@ -0,0 +1,33 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTest {
+
+    @Test
+    void testLogger() {
+        Logger logger = new Logger();
+
+        boolean actual1 = logger.shouldPrintMessage(1, "hello");
+        boolean expected1 = true;
+        assertEquals(expected1, actual1);
+
+        boolean actual2 = logger.shouldPrintMessage(5, "hello");
+        boolean expected2 = false;
+        assertEquals(expected2, actual2);
+
+        boolean actual3 = logger.shouldPrintMessage(5, "hi");
+        boolean expected3 = true;
+        assertEquals(expected3, actual3);
+
+        boolean actual4 = logger.shouldPrintMessage(12,"hi");
+        boolean expected4 = false;
+        assertEquals(expected4, actual4);
+
+        boolean actual5 = logger.shouldPrintMessage(12,"hello");
+        boolean expected5 = true;
+        assertEquals(expected5, actual5);
+    }
+}
