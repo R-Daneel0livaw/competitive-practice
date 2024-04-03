76c6c87e383b292f03813f909430dcecdb8d0b2f
erich <eric.hine2214@gmail.com>
2024-04-02 21:46:14 -0400

    Add RemoveAdjacentDuplicates (1047) Easy/Stack

---
+++ src/main/java/org/hine/easy/RemoveAdjacentDuplicates.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+public class RemoveAdjacentDuplicates {
+    public String removeAdjacentDuplicates(String s) {
+        var sb = new StringBuilder();
+        var sbLength = 0;
+
+        for(char c : s.toCharArray()) {
+            if(sbLength != 0 && c == sb.charAt(sbLength - 1)) {
+                sb.deleteCharAt(sbLength-- - 1);
+            } else {
+                sb.append(c);
+                sbLength++;
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/org/hine/easy/RemoveAdjacentDuplicatesTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RemoveAdjacentDuplicatesTest {
+
+    @Test
+    void testRemoveAdjacentDuplicates() {
+        var s1 = "azxxzy";
+        var expected1 = "ay";
+        var actual1 = new RemoveAdjacentDuplicates().removeAdjacentDuplicates(s1);
+
+        assertEquals(expected1, actual1);
+    }
+}
