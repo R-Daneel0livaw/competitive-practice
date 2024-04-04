63bf5a52bcd8d3b3a454c73ca4cefe4828a666dd
erich <eric.hine2214@gmail.com>
2024-04-04 19:51:52 -0400

    Add CrawlerLogFolder (1598) Easy/Stack

---
+++ src/main/java/org/hine/easy/CrawlerLogFolder.java
@@ -0,0 +1,22 @@
+package org.hine.easy;
+
+import java.util.Stack;
+
+public class CrawlerLogFolder {
+
+    public int crawlerLogFolder(String[] ops) {
+        Stack<String> stack = new Stack<>();
+        for (var op : ops) {
+            if(op.equalsIgnoreCase("../")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (op.equalsIgnoreCase("./")) {
+                // no-op; same folder
+            } else {
+                stack.push(op);
+            }
+        }
+        return stack.size();
+    }
+}
---
+++ src/test/java/org/hine/easy/CrawlerLogFolderTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CrawlerLogFolderTest {
+
+    @Test
+    void testCrawlerLogFolder() {
+        String[] ops = new String[]{"d1/","d2/","../","d21/","./"};
+        var expected = 2;
+        var actual = new CrawlerLogFolder().crawlerLogFolder(ops);
+
+        assertEquals(expected, actual);
+    }
+}
