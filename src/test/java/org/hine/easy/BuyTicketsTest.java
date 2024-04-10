1db36f473ca44fdc0412d8a4dd715f756b8931ce
erich <eric.hine2214@gmail.com>
2024-04-09 20:13:52 -0400

    Add BuyTickets (2073) Easy/Queue

---
+++ src/test/java/org/hine/easy/BuyTicketsTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BuyTicketsTest {
+
+    @Test
+    void testBuyTickets() {
+        var tickets = new int[] {5, 1, 1, 1};
+        int k = 0;
+
+        var expected = 8;
+        var actual = new BuyTickets().buyTickets(tickets, k);
+
+        assertEquals(expected, actual);
+    }
+}
