bb967bbd93fc2e22178158e377c5a938b7098fd6
erich <eric.hine2214@gmail.com>
2024-04-09 20:33:07 -0400

    Add MakeArrayZero (2357) Easy/Queue

---
+++ src/main/java/org/hine/easy/MakeArrayZero.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MakeArrayZero {
+
+    public int makeArrayZero(int[] nums) {
+        Set<Integer> a = new HashSet<>();
+        for (int num : nums) {
+            if (num == 0) {
+                continue;
+            }
+            a.add(num);
+        }
+        return a.size();
+    }
+}
---
+++ src/test/java/org/hine/easy/MakeArrayZeroTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MakeArrayZeroTest {
+
+    @Test
+    void testMakeArrayZero() {
+        var nums = new int[] {1, 5, 0, 3, 5};
+
+        var expected = 3;
+        var actual = new MakeArrayZero().makeArrayZero(nums);
+
+        assertEquals(expected, actual);
+    }
+}
