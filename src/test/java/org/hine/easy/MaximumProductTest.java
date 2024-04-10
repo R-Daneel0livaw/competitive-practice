b520fe21726dbfb60d24721a0a7b153c682431ec
erich <eric.hine2214@gmail.com>
2024-04-09 20:22:48 -0400

    Add MaximumProduct (1464) Easy/Queue

---
+++ src/test/java/org/hine/easy/MaximumProductTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MaximumProductTest {
+
+    @Test
+    void testMaximumProduct() {
+        var nums = new int[] {1, 5, 4, 5};
+
+        var expected = 16;
+        var actual = new MaximumProduct().maximumProduct(nums);
+
+        assertEquals(expected, actual);
+    }
+}
