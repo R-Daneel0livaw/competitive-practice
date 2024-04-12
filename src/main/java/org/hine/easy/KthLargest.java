df8959e248c592936257a43884d3608f796be55c
erich <eric.hine2214@gmail.com>
2024-04-12 17:38:16 -0400

    Add KthLargest (703) Easy/Queue

---
+++ src/main/java/org/hine/easy/KthLargest.java
@@ -0,0 +1,30 @@
+package org.hine.easy;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargest {
+    private final int k;
+    private final Queue<Integer> heap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        heap = new PriorityQueue<>();
+
+        Arrays.stream(nums).forEach(heap::offer);
+        shrinkHeap(heap);
+    }
+
+    public int add(int val) {
+        heap.offer(val);
+        shrinkHeap(heap);
+        return heap.peek() != null ? heap.peek() : 0;
+    }
+
+    private void shrinkHeap(Queue<Integer> heap) {
+        while (heap.size() > k) {
+            heap.poll();
+        }
+    }
+}
---
+++ src/test/java/org/hine/easy/KthLargestTest.java
@@ -0,0 +1,35 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KthLargestTest {
+
+    @Test
+    void testKthLargest() {
+        var k = 3;
+        var nums = new int[] {4, 5, 8, 2};
+        var kthLargest = new KthLargest(k, nums);
+
+        var expected1 = 4;
+        var actual1 = kthLargest.add(3);
+        assertEquals(expected1, actual1);
+
+        var expected2 = 5;
+        var actual2 = kthLargest.add(5);
+        assertEquals(expected2, actual2);
+
+        var expected3 = 5;
+        var actual3 = kthLargest.add(10);
+        assertEquals(expected3, actual3);
+
+        var expected4 = 8;
+        var actual4 = kthLargest.add(9);
+        assertEquals(expected4, actual4);
+
+        var expected5 = 8;
+        var actual5 = kthLargest.add(4);
+        assertEquals(expected5, actual5);
+    }
+}
