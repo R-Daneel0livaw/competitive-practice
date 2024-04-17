e50e1c777f2b1eead8fbb87e1723f706d4a9781f
erich <eric.hine2214@gmail.com>
2024-04-17 17:45:54 -0400

    Add DiameterBinaryTree (543) Easy/Tree

---
+++ src/main/java/org/hine/easy/DiameterBinaryTree.java
@@ -0,0 +1,24 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+
+public class DiameterBinaryTree {
+
+    private int diameter;
+    public int diameterBinaryTree(TreeNode root) {
+        this.diameter = 0;
+        longestPath(root);
+        return this.diameter;
+    }
+
+    private int longestPath(TreeNode node) {
+        if (node == null) return 0;
+
+        var left = longestPath(node.left);
+        var right = longestPath(node.right);
+
+        this.diameter = Math.max(this.diameter, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ src/test/java/org/hine/easy/DiameterBinaryTreeTest.java
@@ -0,0 +1,21 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiameterBinaryTreeTest {
+
+    @Test
+    void testDiameterBinaryTree() {
+        var root = new TreeNode(1,
+                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                new TreeNode(3));
+
+        var expected = 3;
+        var actual = new DiameterBinaryTree().diameterBinaryTree(root);
+
+        assertEquals(expected, actual);
+    }
+}
