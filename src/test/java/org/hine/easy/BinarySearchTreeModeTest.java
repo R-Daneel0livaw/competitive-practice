2b718f89c9833323f92e4830a697c3693c0cbded
erich <eric.hine2214@gmail.com>
2024-04-17 18:05:21 -0400

    Add BinarySearchTreeMode (501) Easy/Tree

---
+++ src/test/java/org/hine/easy/BinarySearchTreeModeTest.java
@@ -0,0 +1,21 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinarySearchTreeModeTest {
+
+    @Test
+    void testBinarySearchTreeMode() {
+        var root = new TreeNode(1,
+                null,
+                new TreeNode(2, new TreeNode(2), null));
+
+        var expected = new int[] {2};
+        var actual = new BinarySearchTreeMode().bstMode(root);
+
+        assertArrayEquals(expected, actual);
+    }
+}
