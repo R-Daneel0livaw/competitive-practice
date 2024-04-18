9e4e16730035e2158290a1f9cd87341814c07755
erich <eric.hine2214@gmail.com>
2024-04-18 18:18:15 -0400

    Add ClosestBSTValue (270) Easy/Tree

---
+++ src/main/java/org/hine/easy/ClosestBSTValue.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+
+public class ClosestBSTValue {
+
+    public int closestBSTValue(TreeNode root, double target) {
+        int val;
+        var closest = root.val;
+        while (root != null) {
+            val = root.val;
+            closest = Math.abs(val - target) < Math.abs(closest - target)
+                    || (Math.abs(val - target) == Math.abs(closest - target) && val < closest) ? val : closest;
+            root = target < root.val ? root.left : root.right;
+        }
+        return closest;
+    }
+}
---
+++ src/test/java/org/hine/easy/ClosestBSTValueTest.java
@@ -0,0 +1,22 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClosestBSTValueTest {
+
+    @Test
+    void testClosestBSTValue() {
+        var root = new TreeNode(4,
+                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(5));
+        var target = 3.714286;
+
+        var expected = 4;
+        var actual = new ClosestBSTValue().closestBSTValue(root, target);
+
+        assertEquals(expected, actual);
+    }
+}
