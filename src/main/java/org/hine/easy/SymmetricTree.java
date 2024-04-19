66dc59b8e993efbc4b0ddfb6f9fe36fc8a11efbf
erich <eric.hine2214@gmail.com>
2024-04-19 15:14:34 -0400

    Add SymmetricTree (101) Easy/Tree

---
+++ src/main/java/org/hine/easy/SymmetricTree.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+
+public class SymmetricTree {
+
+    public boolean symmetricTree(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+
+        return t1.val == t2.val
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+}
---
+++ src/test/java/org/hine/easy/SymmetricTreeTest.java
@@ -0,0 +1,21 @@
+package org.hine.easy;
+
+import org.hine.easy.util.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SymmetricTreeTest {
+
+    @Test
+    void testSymmetricTree() {
+        var root = new TreeNode(1,
+                new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+
+        var expected = true;
+        var actual = new SymmetricTree().symmetricTree(root);
+
+        assertEquals(expected, actual);
+    }
+}
