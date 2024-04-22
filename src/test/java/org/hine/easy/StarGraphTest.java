dcf44d667c7c8b3420027739a542095ef2c92ee7
erich <eric.hine2214@gmail.com>
2024-04-22 17:44:08 -0400

    Add StarGraph (1791) Easy/Tree

---
+++ src/test/java/org/hine/easy/StarGraphTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StarGraphTest {
+
+    @Test
+    void testStartGraph() {
+        int[][] edges = {{1, 2}, {2, 3}, {4, 2}};
+
+        var expected = 2;
+        var actual = new StarGraph().starGraph(edges);
+
+        assertEquals(expected, actual);
+    }
+}
