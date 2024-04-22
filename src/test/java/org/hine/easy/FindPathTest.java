477545c0ef2d4466156b40dcf00e52902bbc82c2
erich <eric.hine2214@gmail.com>
2024-04-22 18:02:02 -0400

    Add FindPath (1971) Easy/Tree

---
+++ src/test/java/org/hine/easy/FindPathTest.java
@@ -0,0 +1,21 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FindPathTest {
+
+    @Test
+    void testFindPath() {
+        int[][] edges = {{0, 1}, {1, 2}, {2, 0}};
+        var n = 3;
+        var source = 0;
+        var destination = 2;
+
+        var expected = true;
+        var actual = new FindPath().findPath(n, edges, source, destination);
+
+        assertEquals(expected, actual);
+    }
+}
