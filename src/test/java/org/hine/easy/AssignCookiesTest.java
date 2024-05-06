399adefaedcd3192a4dffabb279e2b51041a76e4
erich <eric.hine2214@gmail.com>
2024-05-06 15:28:24 -0400

    Add AssignCookies (455) Easy/Sorting

---
+++ src/test/java/org/hine/easy/AssignCookiesTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssignCookiesTest {
+
+    @Test
+    void tetAssignCookies() {
+        var g = new int[]{1, 2};
+        var s = new int[]{1, 2, 3};
+
+        var expected = 2;
+        var actual = new AssignCookies().assignCookies(g, s);
+
+        assertEquals(expected, actual);
+    }
+}
