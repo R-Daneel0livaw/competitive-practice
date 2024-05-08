0eede8d114367302516df05cca09f6a1ee85a97a
erich <eric.hine2214@gmail.com>
2024-05-08 18:23:03 -0400

    Add BuyTwoChocolates (2706) Easy/Sorting

---
+++ src/test/java/org/hine/easy/BuyTwoChocolatesTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BuyTwoChocolatesTest {
+
+    @Test
+    void testBuyTwoChocolates() {
+        var prices = new int[]{1, 2, 2};
+        var money = 3;
+
+        var expected = 0;
+        var actual = new BuyTwoChocolates().buyTwoChocolates(prices, money);
+
+        assertEquals(expected, actual);
+    }
+}
