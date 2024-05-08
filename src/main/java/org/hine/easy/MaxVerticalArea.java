d44a0343da3e2b5087cbf178857b09e5d43b75a6
erich <eric.hine2214@gmail.com>
2024-05-07 20:44:27 -0400

    Add MaxVerticalArea (1637) Easy/Sorting

---
+++ src/main/java/org/hine/easy/MaxVerticalArea.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MaxVerticalArea {
+
+    public int maxVerticalArea(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+
+        var ans = 0;
+        for (int i = 1; i < points.length; i++) {
+            ans = Math.max(ans, points[i][0] - points[i - 1][0]);
+        }
+        return ans;
+    }
+}
---
+++ src/test/java/org/hine/easy/MaxVerticalAreaTest.java
@@ -0,0 +1,25 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MaxVerticalAreaTest {
+
+    @Test
+    void testMaxVerticalArea() {
+        int[][] points = {
+                {3, 1},
+                {9, 0},
+                {1, 0},
+                {1, 4},
+                {5, 3},
+                {8, 8}
+        };
+
+        var expected = 3;
+        var actual = new MaxVerticalArea().maxVerticalArea(points);
+
+        assertEquals(expected, actual);
+    }
+}
