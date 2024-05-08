ff38f6f64e08af1af02d9b3480891fa9b8466c8d
erich <eric.hine2214@gmail.com>
2024-05-08 18:12:45 -0400

    Add MaxProductPairs (1913) Easy/Sorting

---
+++ src/main/java/org/hine/easy/MaxProductPairs.java
@@ -0,0 +1,26 @@
+package org.hine.easy;
+
+public class MaxProductPairs {
+
+    public int maxProductPairs(int[] nums) {
+        var biggest = 0; var secondBiggest = 0;
+        var smallest = Integer.MAX_VALUE; var secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > biggest) {
+                secondBiggest = biggest;
+                biggest = num;
+            } else {
+                secondBiggest = Math.max(secondBiggest, num);
+            }
+
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else {
+                secondSmallest = Math.min(secondSmallest, num);
+            }
+        }
+        return (biggest * secondBiggest) - (smallest * secondSmallest);
+    }
+}
---
+++ src/test/java/org/hine/easy/MaxProductPairsTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MaxProductPairsTest {
+
+    @Test
+    void testMaxProductPairs() {
+        var nums = new int[]{4, 2, 5, 9, 7, 4, 8};
+
+        var expected = 64;
+        var actual = new MaxProductPairs().maxProductPairs(nums);
+
+        assertEquals(expected, actual);
+    }
+}
