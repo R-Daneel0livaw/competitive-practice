98abb3abd95db5d487989c016015656b561cca64
erich <eric.hine2214@gmail.com>
2024-05-08 18:35:44 -0400

    Add FairCandySwap (888) Easy/Sorting

---
+++ src/main/java/org/hine/easy/FairCandySwap.java
@@ -0,0 +1,36 @@
+package org.hine.easy;
+
+import java.util.Arrays;
+
+public class FairCandySwap {
+
+    public int[] fairCandySwap(int[] A, int[] B) {
+        var sumAlice = Arrays.stream(A).sum();
+        var sumBob = Arrays.stream(B).sum();
+
+        if (sumAlice - sumBob == 0) return new int[]{0, 0};
+
+        Arrays.sort(B);
+
+        for (int j : A) {
+            var index = binarySearch(B, j, sumAlice, sumBob);
+            if (index != -1) return new int[]{j, B[index]};
+        }
+        return new int[]{0, 0};
+    }
+
+    private int binarySearch(int[] bob, int alice, int sumA, int sumB) {
+        var left = 0; var right = bob.length - 1;
+        while (left <= right) {
+            var pivot = (left + right) / 2;
+            if (sumA - alice + bob[pivot] == sumB - bob[pivot] + alice) {
+                return pivot;
+            } else if (sumA - alice + bob[pivot] > sumB - bob[pivot] + alice) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/org/hine/easy/FairCandySwapTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FairCandySwapTest {
+
+    @Test
+    void testFairCandySwap() {
+        var alice = new int[]{1, 2};
+        var bob = new int[]{2, 3};
+
+        var expected = new int[]{1, 2};
+        var actual = new FairCandySwap().fairCandySwap(alice, bob);
+
+        assertArrayEquals(expected, actual);
+    }
+}
