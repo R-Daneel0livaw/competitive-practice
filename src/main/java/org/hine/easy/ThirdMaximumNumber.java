d916b1417f93e385b199c7e038081d2bd7e4211b
erich <eric.hine2214@gmail.com>
2024-05-09 14:25:56 -0400

    Add ThirdMaximumNumber (414) Easy/Sorting

---
+++ src/main/java/org/hine/easy/ThirdMaximumNumber.java
@@ -0,0 +1,26 @@
+package org.hine.easy;
+
+import java.util.TreeSet;
+
+public class ThirdMaximumNumber {
+
+    public int thirdMaximumNumber(int[] nums) {
+        TreeSet<Integer> sortedNums = new TreeSet<>();
+
+        for (int num : nums) {
+            if (sortedNums.contains(num)) {
+                continue;
+            }
+
+            if (sortedNums.size() == 3) {
+                if (sortedNums.first() < num) {
+                    sortedNums.pollFirst();
+                    sortedNums.add(num);
+                }
+            } else {
+                sortedNums.add(num);
+            }
+        }
+        return sortedNums.size() == 3 ? sortedNums.first() : sortedNums.last();
+    }
+}
---
+++ src/test/java/org/hine/easy/ThirdMaximumNumberTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ThirdMaximumNumberTest {
+
+    @Test
+    void testThirdMaximumNumber() {
+        var nums = new int[] {2, 2, 3, 1};
+
+        var expected = 1;
+        var actual = new ThirdMaximumNumber().thirdMaximumNumber(nums);
+
+        assertEquals(expected, actual);
+    }
+}
