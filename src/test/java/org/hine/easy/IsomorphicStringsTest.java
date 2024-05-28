a5cf41b82bcb0232088e83d2253f9be43d6b92ba
erich <eric.hine2214@gmail.com>
2024-05-28 18:09:25 -0400

    Add IsomorphicStrings (205) Easy/String

---
+++ src/test/java/org/hine/easy/IsomorphicStringsTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IsomorphicStringsTest {
+
+    @Test
+    void testIsomorphicStrings() {
+        var s = "paper";
+        var t = "title";
+
+        var expected = true;
+        var actual = new IsomorphicStrings().isIsomorphic(s, t);
+
+        assertEquals(expected, actual);
+    }
+}
