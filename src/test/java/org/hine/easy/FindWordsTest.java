48a961e9c00e9bfa5a26380801b965e28ac529bb
erich <eric.hine2214@gmail.com>
2024-05-28 18:26:23 -0400

    Add FindWords (1160) Easy/String

---
+++ src/test/java/org/hine/easy/FindWordsTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FindWordsTest {
+
+    @Test
+    void testFindWords() {
+        var words = new String[]{"hello","world","leetcode"};
+        var chars = "welldonehoneyr";
+
+        var expected = 10;
+        var actual = new FindWords().findWords(words, chars);
+
+        assertEquals(expected, actual);
+    }
+}
