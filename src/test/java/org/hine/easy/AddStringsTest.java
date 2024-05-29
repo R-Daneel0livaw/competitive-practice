c5f129c6921bbb70221d1c38a7c0b6600858db9f
erich <eric.hine2214@gmail.com>
2024-05-29 18:20:46 -0400

    Add AddStrings (415) Easy/String

---
+++ src/test/java/org/hine/easy/AddStringsTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddStringsTest {
+
+    @Test
+    void testAddStrings() {
+        var num1 = "456";
+        var num2 = "77";
+
+        var expected = "533";
+        var actual = new AddStrings().addStrings(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+}
