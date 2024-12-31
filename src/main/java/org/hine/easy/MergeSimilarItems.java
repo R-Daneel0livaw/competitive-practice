8950a69e30f0aa602286b2c6ffe78a765d9b0704
erich <eric.hine2214@gmail.com>
2024-12-31 16:13:02 -0500

    Cleanup MergeSimilarItems (2363) Easy/Set

--- src/main/java/org/hine/easy/MergeSimilarItems.java
+++ src/main/java/org/hine/easy/MergeSimilarItems.java
@@ -6,13 +6,8 @@ public class MergeSimilarItems {
 
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
         Map<Integer, Integer> cnt = new TreeMap<>();
-        for(int[] it : items1) {
-            cnt.merge(it[0], it[1], Integer::sum);
-        }
-
-        for(int[] it : items2) {
-            cnt.merge(it[0], it[1], Integer::sum);
-        }
+        Arrays.stream(items1).forEach(it -> cnt.merge(it[0], it[1], Integer::sum));
+        Arrays.stream(items2).forEach(it -> cnt.merge(it[0], it[1], Integer::sum));
 
         List<List<Integer>> ret = new ArrayList<>();
         for(var e : cnt.entrySet()) {
