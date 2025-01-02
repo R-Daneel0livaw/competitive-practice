1f8cdb0e344a879360c2043546fccd38da2c80da
erich <eric.hine2214@gmail.com>
2025-01-02 16:39:06 -0500

    Add AddBinary (67) Easy/Bit Manipulation

---
+++ src/test/java/org/hine/easy/AddBinaryTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddBinaryTest {
+
+    @Test
+    void testAddBinary() {
+        var a = "11";
+        var b = "1";
+
+        var expected = "100";
+        var actual = new AddBinary().addBinary(a, b);
+
+        assertEquals(expected, actual);
+    }
+}
