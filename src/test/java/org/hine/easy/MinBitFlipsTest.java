6101f033b42a588c02e98e1e473d82f90abac66e
erich <eric.hine2214@gmail.com>
2025-01-03 16:43:23 -0500

    Add MinBitFlips (2220) Easy/Bit Manipulation

---
+++ src/test/java/org/hine/easy/MinBitFlipsTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinBitFlipsTest {
+
+    @Test
+    void testMinBitFlips() {
+        var start = 10;
+        var goal = 7;
+        var expected = 3;
+        var actual = new MinBitFlips().minBitFlips(start, goal);
+        assertEquals(expected, actual);
+    }
+}
