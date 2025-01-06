84ce30025f15bf640eb71f82d6fdceadeae51b7c
erich <eric.hine2214@gmail.com>
2025-01-06 17:27:17 -0500

    Add ConsistentStrings (1684) Easy/Bit Manipulation

---
+++ src/main/java/org/hine/easy/ConsistentStrings.java
@@ -0,0 +1,27 @@
+package org.hine.easy;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ConsistentStrings {
+
+    public int consistentStrings(String allowed, String[] words) {
+        var allowedChars = IntStream.range(0, allowed.length()).mapToObj(allowed::charAt)
+                .collect(Collectors.toSet());
+
+        var consistent = 0;
+
+        for (String word : words) {
+            var isConsistent = true;
+            for (int i = 0; i < word.length(); i++) {
+                var currentChart = word.charAt(i);
+                if (!allowedChars.contains(currentChart)) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            if (isConsistent) consistent++;
+        }
+        return consistent;
+    }
+}
---
+++ src/test/java/org/hine/easy/ConsistentStringsTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConsistentStringsTest {
+
+    @Test
+    void testConsistentStrings() {
+        var allowed = "ab";
+        var words = new String[] {"ad", "bd", "aaab", "baa", "badab"};
+        var expected = 2;
+        var actual = new ConsistentStrings().consistentStrings(allowed, words);
+
+        assertEquals(expected, actual);
+    }
+}
