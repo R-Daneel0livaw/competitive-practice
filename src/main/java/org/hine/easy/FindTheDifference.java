5df241f05c81a05cb48d2e5ab696b5be929a7a13
erich <eric.hine2214@gmail.com>
2025-01-07 18:08:07 -0500

    Add FindTheDifference (389) Easy/Bit Manipulation

---
+++ src/main/java/org/hine/easy/FindTheDifference.java
@@ -0,0 +1,16 @@
+package org.hine.easy;
+
+public class FindTheDifference {
+
+    public char findTheDifference(String s, String t) {
+        char ch = 0;
+        for (char c : s.toCharArray()) {
+            ch ^= c;
+        }
+
+        for (char c : t.toCharArray()) {
+            ch ^= c;
+        }
+        return ch;
+    }
+}
---
+++ src/test/java/org/hine/easy/FindTheDifferenceTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindTheDifferenceTest {
+
+    @Test
+    void testFindTheDifference() {
+        var s = "abcd";
+        var t = "abcde";
+        var expected = 'e';
+        var actual = new FindTheDifference().findTheDifference(s, t);
+
+        assertEquals(expected, actual);
+    }
+}
