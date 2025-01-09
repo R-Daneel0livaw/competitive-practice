1fb069244a6b6c2d5276ee1d5a64588d3d9f9158
erich <eric.hine2214@gmail.com>
2025-01-09 16:16:09 -0500

    Add ReverseBits (190) Easy/Bit Manipulation

---
+++ src/main/java/org/hine/easy/ReverseBits.java
@@ -0,0 +1,14 @@
+package org.hine.easy;
+
+public class ReverseBits {
+
+    public int reverseBits(int n) {
+        var ret = 0; var power = 31;
+        while (n != 0) {
+            ret += (n & 1) << power;
+            n = n >>> 1;
+            power -= 1;
+        }
+        return ret;
+    }
+}
---
+++ src/test/java/org/hine/easy/ReverseBitsTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseBitsTest {
+
+    @Test
+    void testReverseBits() {
+        var n = 0b00000010100101000001111010011100;
+        var expected = 0b00111001011110000010100101000000;
+        var actual = new ReverseBits().reverseBits(n);
+
+        assertEquals(expected, actual);
+    }
+}
