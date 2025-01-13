6447f8a4bbc6eac88cd52d385d30f769f377fdac
erich <eric.hine2214@gmail.com>
2025-01-13 17:25:05 -0500

    Add LuckyNumbersMatrix (1380) Easy/Matrix

---
+++ src/main/java/org/hine/easy/LuckyNumbersMatrix.java
@@ -0,0 +1,33 @@
+package org.hine.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyNumbersMatrix {
+
+    public List<Integer> luckyNumbersMatrix(int[][] matrix) {
+        var n = matrix.length;
+        var m = matrix[0].length;
+        List<Integer> lucky = new ArrayList<>();
+
+        for (int[] value : matrix) {
+            var num = Integer.MAX_VALUE;
+            var index = -1;
+            for (int j = 0; j < m; j++) {
+                if (value[j] < num) {
+                    num = value[j];
+                    index = j;
+                }
+            }
+            var flag = true;
+            for (int[] ints : matrix) {
+                if (ints[index] > num) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) lucky.add(num);
+        }
+        return lucky;
+    }
+}
---
+++ src/test/java/org/hine/easy/LuckyNumbersMatrixTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LuckyNumbersMatrixTest {
+
+    @Test
+    void testLuckyNumbersMatrix() {
+        var matrix = new int[][] {{3, 7, 8}, {9, 11, 13}, {15, 16, 17}};
+        var expected = List.of(15);
+        var actual = new LuckyNumbersMatrix().luckyNumbersMatrix(matrix);
+        assertEquals(expected, actual);
+    }
+}
