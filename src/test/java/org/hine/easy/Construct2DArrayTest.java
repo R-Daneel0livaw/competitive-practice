c70ab857be8d8dcc57568a5ceb3a2cf852806dca
erich <eric.hine2214@gmail.com>
2025-01-13 17:46:57 -0500

    Add Construct2DArray (2022) Easy/Matrix

---
+++ src/test/java/org/hine/easy/Construct2DArrayTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class Construct2DArrayTest {
+
+    @Test
+    void testConstruct2DArray() {
+        var original = new int[] {1, 2, 3, 4};
+        var m = 2; var n = 2;
+        var expected = new int[][] {{1, 2}, {3, 4}};
+        var actual = new Construct2DArray().construct2DArray(original, m, n);
+
+        assertArrayEquals(expected, actual);
+    }
+}
