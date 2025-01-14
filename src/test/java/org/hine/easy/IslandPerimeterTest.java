6302ea9554de8178379e847467a74b7ce33f7fa5
erich <eric.hine2214@gmail.com>
2025-01-14 17:39:50 -0500

    Add IslandPerimeter (463) Easy/Matrix

---
+++ src/test/java/org/hine/easy/IslandPerimeterTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IslandPerimeterTest {
+
+    @Test
+    void testIslandPerimeter() {
+        var grid = new int[][] {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        var expected = 16;
+        var actual = new IslandPerimeter().islandPerimeter(grid);
+
+        assertEquals(expected, actual);
+    }
+}
