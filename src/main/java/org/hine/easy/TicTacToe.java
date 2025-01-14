04e110f51c806e9954438876a00046d1ae576089
erich <eric.hine2214@gmail.com>
2025-01-14 17:22:52 -0500

    Add TicTacToe (1275) Easy/Matrix

---
+++ src/main/java/org/hine/easy/TicTacToe.java
@@ -0,0 +1,33 @@
+package org.hine.easy;
+
+public class TicTacToe {
+
+    public String ticTacToe(int[][] moves) {
+        var n = 3;
+        var rows = new int[n];
+        var cols = new int[n];
+        var diag = 0;
+        var antiDiag = 0;
+        var player = 1;
+
+        for (int[] move : moves) {
+            var row = move[0];
+            var col = move[1];
+
+            rows[row] += player;
+            cols[col] += player;
+
+            if (row == col) diag += player;
+
+            if (row + col == n - 1) antiDiag += player;
+
+            if (Math.abs(rows[row]) == n || Math.abs(cols[col]) == n
+                    || Math.abs(diag) == n || Math.abs(antiDiag) == n) {
+                return player == 1 ? "A" : "B";
+            }
+
+            player *= -1;
+        }
+        return moves.length == n * n ? "Draw" : "Pending";
+    }
+}
---
+++ src/test/java/org/hine/easy/TicTacToeTest.java
@@ -0,0 +1,16 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TicTacToeTest {
+
+    @Test
+    void testTicTacToe() {
+        var moves = new int[][] {{0, 0}, {2, 0}, {1, 1}, {2, 1}, {2, 2}};
+        var expected = "A";
+        var actual = new TicTacToe().ticTacToe(moves);
+        assertEquals(expected, actual);
+    }
+}
