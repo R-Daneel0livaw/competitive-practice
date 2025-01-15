94b5ce9e0b0223b6071ed9e02f0dfe61a21af550
erich <eric.hine2214@gmail.com>
2025-01-15 17:15:59 -0500

    Add ToeplitzMatrix (766) Easy/Matrix

---
+++ src/test/java/org/hine/easy/ToeplitzMatrixTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToeplitzMatrixTest {
+
+    @Test
+    void testToeplitzMatrix() {
+        var matrix = new int[][] {{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}};
+        var expected = true;
+        var actual = new ToeplitzMatrix().isToeplitzMatrix(matrix);
+
+        assertEquals(expected, actual);
+    }
+}
