1737a56e496aab40f8d1394140e0c1852e07f390
erich <eric.hine2214@gmail.com>
2025-01-18 16:24:42 -0500

    Add MatrixRotation (1886) Easy/Matrix

---
+++ src/main/java/org/hine/easy/MatrixRotation.java
@@ -0,0 +1,39 @@
+package org.hine.easy;
+
+public class MatrixRotation {
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+
+        for (int rotations = 0; rotations < 4; rotations++) {
+            if (areMatricesEqual(mat, target)) {
+                return true;
+            }
+            mat = rotate90Degrees(mat);
+        }
+
+        return false;
+    }
+
+    private boolean areMatricesEqual(int[][] mat, int[][] target) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != target[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private int[][] rotate90Degrees(int[][] mat) {
+        int n = mat.length;
+        int[][] rotated = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = mat[i][j];
+            }
+        }
+        return rotated;
+    }
+}
---
+++ src/test/java/org/hine/easy/MatrixRotationTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatrixRotationTest {
+
+    @Test
+    void testMatrixRotation() {
+        var mat = new int[][] {{0, 1}, {1, 0}};
+        var target = new int[][] {{1, 0}, {0, 1}};
+        var expected = true;
+        var actual = new MatrixRotation().findRotation(mat, target);
+
+        assertEquals(expected, actual);
+    }
+}
