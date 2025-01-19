be85f458acc17fbee2a97ac785ee1730462ca257
erich <eric.hine2214@gmail.com>
2025-01-19 14:25:54 -0500

    Add FloodFill (733) Easy/Matrix

---
+++ src/test/java/org/hine/easy/FloodFillTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class FloodFillTest {
+
+    @Test
+    void testFloodFill() {
+        var image = new int[][] {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        var sr = 1; var sc = 1; var color = 2;
+        var expected = new int[][] {{2, 2, 2}, {2, 2, 0}, {2, 0, 1}};
+        var actual = new FloodFill().floodFill(image, sr, sc, color);
+
+        assertArrayEquals(expected, actual);
+    }
+}
