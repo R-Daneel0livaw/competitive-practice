5e8963f73b85d5909793385ecd82180204cbb42d
erich <eric.hine2214@gmail.com>
2025-02-04 19:40:41 -0500

    Add PalindromeInteger (9) Easy/Math

---
+++ src/main/java/org/hine/easy/PalindromeInteger.java
@@ -0,0 +1,16 @@
+package org.hine.easy;
+
+public class PalindromeInteger {
+
+    public boolean palindromeInteger(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        var revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
---
+++ src/test/java/org/hine/easy/PalindromeIntegerTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PalindromeIntegerTest {
+
+    @Test
+    void testPalindromeInteger() {
+        var x = 121;
+        var expected = true;
+        var actual = new PalindromeInteger().palindromeInteger(x);
+
+        assertEquals(expected, actual);
+    }
+}
