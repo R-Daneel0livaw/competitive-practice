21d7fafbc218e2d446433900914cc9c08c384226
erich <eric.hine2214@gmail.com>
2025-02-04 19:47:01 -0500

    Add AddTwoIntegers (2235) Easy/Math

---
+++ src/main/java/org/hine/easy/AddTwoIntegers.java
@@ -0,0 +1,8 @@
+package org.hine.easy;
+
+public class AddTwoIntegers {
+
+    public int addTwoIntegers(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/org/hine/easy/AddTwoIntegersTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddTwoIntegersTest {
+
+    @Test
+    void testAddTwoIntegers() {
+        var num1 = 12;
+        var num2 = 5;
+        var expected = 17;
+        var actual = new AddTwoIntegers().addTwoIntegers(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+}
