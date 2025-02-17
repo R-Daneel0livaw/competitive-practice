ebe3c450303820c48f70a11fa327139d5ce76d94
erich <eric.hine2214@gmail.com>
2025-02-17 16:29:22 -0500

    Add ClimbStairs (70) Easy/Math

---
+++ src/main/java/org/hine/easy/ClimbStairs.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        var memo = new int[n + 1];
+        return climbStairsHelper(0, n, memo);
+    }
+
+    private int climbStairsHelper(int i, int n, int[] memo) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+        if (memo[i] > 0) return memo[i];
+
+        memo[i] = climbStairsHelper(i + 1, n, memo) + climbStairsHelper(i + 2, n, memo);
+        return memo[i];
+    }
+}
---
+++ src/test/java/org/hine/easy/ClimbStairsTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClimbStairsTest {
+
+    @Test
+    void testClimbStairs() {
+        var n = 2;
+        var expected = 2;
+        var actual = new ClimbStairs().climbStairs(n);
+
+        assertEquals(expected, actual);
+    }
+}
