5b15abcedded16811e53425fe4da2c67b1e510b0
erich <eric.hine2214@gmail.com>
2025-02-18 18:17:39 -0500

    Add FinalArrayState (3264) Easy/Math

---
+++ src/test/java/org/hine/easy/FinalArrayStateTest.java
@@ -0,0 +1,19 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class FinalArrayStateTest {
+
+    @Test
+    void testFinalArrayState() {
+        var nums = new int[] {2, 1, 3, 5, 6};
+        var k = 5;
+        var multiplier = 2;
+        var expected = new int[] {8, 4, 6, 5, 6};
+        var actual = new FinalArrayState().finalArrayState(nums, k, multiplier);
+
+        assertArrayEquals(expected, actual);
+    }
+}
