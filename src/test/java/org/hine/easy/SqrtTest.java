678a421a66ec5328ce43a848b682aa6f915f4ac5
erich <eric.hine2214@gmail.com>
2025-02-18 17:56:03 -0500

    Add Sqrt (69) Easy/Math

---
+++ src/test/java/org/hine/easy/SqrtTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SqrtTest {
+
+    @Test
+    void testSqrt() {
+        var x = 8;
+        var expected =  2;
+        var actual = new Sqrt().sqrt(x);
+
+        assertEquals(expected, actual);
+    }
+}
