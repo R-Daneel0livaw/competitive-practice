afa96eacc096cb214d5170146dc52993019cc133
erich <eric.hine2214@gmail.com>
2025-02-20 18:07:18 -0500

    Add ExcelSheetColumnTitle (168) Easy/Math

---
+++ src/main/java/org/hine/easy/ExcelSheetColumnTitle.java
@@ -0,0 +1,15 @@
+package org.hine.easy;
+
+public class ExcelSheetColumnTitle {
+
+    public String excelSheetColumnTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            ans.append((char) (((columnNumber) % 26) + 'A'));
+            columnNumber /= 26;
+        }
+        return ans.reverse().toString();
+    }
+}
---
+++ src/test/java/org/hine/easy/ExcelSheetColumnTitleTest.java
@@ -0,0 +1,17 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExcelSheetColumnTitleTest {
+
+    @Test
+    void testExcelSheetColumnTitle() {
+        var columnNumber = 28;
+        var expected = "AB";
+        var actual = new ExcelSheetColumnTitle().excelSheetColumnTitle(columnNumber);
+
+        assertEquals(expected, actual);
+    }
+}
