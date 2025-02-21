4db690661df6b54974dfe16a1d748ed0d56f86eb
erich <eric.hine2214@gmail.com>
2025-02-21 18:10:04 -0500

    Add PassThePillow (2582) Easy/Math

---
+++ src/main/java/org/hine/easy/PassThePillow.java
@@ -0,0 +1,12 @@
+package org.hine.easy;
+
+public class PassThePillow {
+
+    public int passThePillow(int n, int time) {
+        var fullRounds = time / (n - 1);
+        var extraTime = time % (n - 1);
+
+        if (fullRounds % 2 == 0) return extraTime + 1;
+        else return n - extraTime;
+    }
+}
---
+++ src/test/java/org/hine/easy/PassThePillowTest.java
@@ -0,0 +1,18 @@
+package org.hine.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassThePillowTest {
+
+    @Test
+    void testPassThePillow() {
+        var n = 4;
+        var time = 5;
+        var expected = 2;
+        var actual = new PassThePillow().passThePillow(n, time);
+
+        assertEquals(expected, actual);
+    }
+}
