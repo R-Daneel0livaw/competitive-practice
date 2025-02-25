bad400cd4a9af5ad63474f99d877f29ff09cf9c7
erich <eric.hine2214@gmail.com>
2025-02-25 18:33:34 -0500

    Add MeetingRooms (252) Easy/Array

---
+++ src/main/java/org/hine/easy/MeetingRooms.java
@@ -0,0 +1,15 @@
+package org.hine.easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MeetingRooms {
+
+    public boolean meetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) return false;
+        }
+        return true;
+    }
+}
